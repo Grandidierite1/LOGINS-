233fa0b885d83d72c66914b155a69f01396d947b
Igor Rodrigues <48895884+Grandidierite1@users.noreply.github.com>
2020-03-09 20:05:29 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,145 @@
+<?php
+// Conexão
+require_once 'db_connect.php';
+
+// Sessão
+session_start();
+
+// Botão enviar
+if(isset($_POST['btn-entrar'])):
+	$erros = array();
+	$login = mysqli_escape_string($connect, $_POST['login']);
+	$senha = mysqli_escape_string($connect, $_POST['senha']);
+
+	if(isset($_POST['lembrar-senha'])):
+
+		setcookie('login', $login, time()+3600);
+		setcookie('senha', md5($senha), time()+3600);
+	endif;
+
+	if(empty($login) or empty($senha)):
+		$erros[] = "<li> O campo login/senha precisa ser preenchido </li>";
+	else:
+		// 105 OR 1=1 
+	    // 1; DROP TABLE teste
+
+		$sql = "SELECT login FROM usuarios WHERE login = '$login'";
+		$resultado = mysqli_query($connect, $sql);		
+
+		if(mysqli_num_rows($resultado) > 0):
+		$senha = md5($senha);       
+		$sql = "SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha'";
+
+
+
+		$resultado = mysqli_query($connect, $sql);
+
+			if(mysqli_num_rows($resultado) == 1):
+				$dados = mysqli_fetch_array($resultado);
+				mysqli_close($connect);
+				$_SESSION['logado'] = true;
+				$_SESSION['id_usuario'] = $dados['id'];
+				header('Location: home.php');
+			else:
+				$erros[] = "<li> Usuário e senha não conferem </li>";
+			endif;
+
+		else:
+			$erros[] = "<li> Usuário inexistente </li>";
+		endif;
+
+	endif;
+
+endif;
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<title>Login V1</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+<?php 
+if(!empty($erros)):
+	foreach($erros as $erro):
+		echo $erro;
+	endforeach;
+endif;
+?>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-pic js-tilt" data-tilt>
+					<img src="images/img-01.png" alt="IMG">
+				</div>
+
+				<form class="login100-form validate-form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+					<span class="login100-form-title">
+						Sistema Web
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "Preencha o campo com o tipo de usuario">
+						<input class="input100" type="text" placeholder="usuario" name="login" value="<?php echo isset($_COOKIE['login']) ? $_COOKIE['login'] : '' ?>"><br>
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate = "Insira sua senha">
+						<input class="input100" type="password" placeholder="senha" name="senha" value="<?php echo isset($_COOKIE['senha']) ? $_COOKIE['senha'] : '' ?>"><br>
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn" type="submit" name="btn-entrar">Entrar</button>
+						
+					</div>
+
+
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+<!--===============================================================================================-->	
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/tilt/tilt.jquery.min.js"></script>
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
